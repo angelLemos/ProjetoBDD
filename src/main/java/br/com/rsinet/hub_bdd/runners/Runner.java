fbd8cd42d67fcfb5302487b2aed21286748819ee
Angelica <angelica.lemos@live.com>
2020-01-28 14:06:15 -0200

    refatoracao

--- src/main/java/br/com/rsinet/hub_bdd/runners/Runner.java
+++ src/main/java/br/com/rsinet/hub_bdd/runners/Runner.java
@@ -16,7 +16,7 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(
 		features = "src/test/resources/features",
 		glue= {"br.com.rsinet.hub_bdd.steps"},
-		//tags = {"@CriarCadastroComSucesso"},
+		tags = {"@CriarCadastroComSucesso"},
 		plugin = {"pretty" , "com.cucumber.listener.ExtentCucumberFormatter:Reports/reportTestes.html"},
 	
 		//exibir a saída do console de maneira muito legível
