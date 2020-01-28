da5a86b5f97bbb95ad9c45ebc5ff429018949a02
Angelica <angelica.lemos@live.com>
2020-01-28 14:36:12 -0200

    refatoracao

--- src/main/java/br/com/rsinet/hub_bdd/utils/ScreenshotUtils.java
+++ src/main/java/br/com/rsinet/hub_bdd/utils/ScreenshotUtils.java
@@ -2,22 +2,14 @@ package br.com.rsinet.hub_bdd.utils;
 
 import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 
 import org.apache.commons.io.FileUtils;
-import org.junit.Rule;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 public class ScreenshotUtils {
 
-//	@Rule
-//	public static String getTimeStamp() {
-//		String timestamp = new SimpleDateFormat("ddMMyyyy_HHmmss").format(Calendar.getInstance().getTime());
-//		return timestamp;
-//	}
 
 	public static String getScreenshot(WebDriver driver, String nomeScreen) {
 		TakesScreenshot ts = (TakesScreenshot) driver;
